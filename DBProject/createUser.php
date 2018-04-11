3e8a79ba90ff38bda75a4084c232dc518a03de2f
mhh5re <31773259+mhh5re@users.noreply.github.com>
2018-04-11 17:41:48 -0400

    Created create user to add user to login

---
+++ DBProject/createUser.php
@@ -0,0 +1,19 @@
+<?php
+ include_once("./library.php"); // To connect to the database
+ $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+ // Check connection
+ if (mysqli_connect_errno())
+ {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+ // Form the SQL query (an INSERT query)
+ $sql="INSERT INTO website_users (username, password)
+ VALUES
+ ('$_POST[username]','$_POST[password]')";
+
+ if (!mysqli_query($con,$sql))
+ {
+ die('Error: ' . mysqli_error($con));
+ }
+ echo "1 record added"; // Output to user
+ mysqli_
