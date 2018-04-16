111ae94cec9be8c8add4df72073022b2e16aae83
mhh5re <31773259+mhh5re@users.noreply.github.com>
2018-04-16 17:44:38 -0400

    username and password functionality

---
+++ DBProject/checkUser.php
@@ -0,0 +1,28 @@
+<?php
+    require_once('./library.php');
+     $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+     // Check connection
+     if (mysqli_connect_errno()) {
+           echo("Can't connect to MySQL Server. Error code: " .
+mysqli_connect_error());
+           return null;
+     }
+
+    $query=mysqli_query($con,"SELECT * FROM website_users WHERE username='$_POST[usernameBox]' && password='$_POST[passwordBox]'");
+    $count=mysqli_num_rows($query);
+    $row=mysqli_fetch_array($query);
+
+    if ($count==1)
+    {
+        session_start();
+        $_SESSION['username'] = $_POST['usernameBox'];
+        $_SESSION['password'] = $_POST['passwordBox'];
+        echo "Success";
+        }
+    else
+    {
+        echo "Invalid username or password";
+        }   
+
+    mysqli_close($con);
+    ?>
