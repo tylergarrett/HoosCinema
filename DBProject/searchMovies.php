dcbdb920c3b014aabded599e181721e1447a9a4b
tylergarrett <tjg3ea@virginia.edu>
2018-04-21 15:36:08 -0400

    Fixed SQL error on index.php

--- DBProject/searchMovies.php
+++ DBProject/searchMovies.php
@@ -9,9 +9,9 @@
                 $stmt->bind_param(s, $searchString);
                 $stmt->execute();
                 $stmt->bind_result($movie_id, $title, $rating, $run_time, $release_year, $director_id, $genre_id);
-                echo "<table border=1><th>movie_id</th><th>title</th><th>rating</th><th>run_time</th><th>release_year</th><th>director_id</th><th>genre_id</th>\n";
+                echo "<table border=1><th>Movie ID</th><th>Title</th><th>Rating</th><th>Run Time</th><th>Release Year</th><th>Director ID</th><th>Genre ID</th>\n";
                 while($stmt->fetch()) {
-                        echo "<tr><td>$movie_id</td><td>$title</td><td>$rating</td><td>$run_time</td><td>$release_year</td><td>$director_id</td><td>genre_id</td></tr>";
+                        echo "<tr><td>$movie_id</td><td>$title</td><td>$rating</td><td>$run_time</td><td>$release_year</td><td>$director_id</td><td>$genre_id</td></tr>";
                 }
                 echo "</table>";
         
